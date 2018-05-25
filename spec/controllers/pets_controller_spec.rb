0c6bcd2157c8ef31859bf54038e8f57f5896538b
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-25 00:55:04 +0000

    automatically backed up by learn

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -92,6 +92,7 @@ describe "Pets Controller" do
       @adam = Owner.create(:name => "Adam")
       visit "/pets/#{@pet.id}/edit"
       choose(@adam.id)
+      binding.pry
       click_button "Update Pet"
       expect(Pet.last.owner.name).to eq("Adam")
     end
